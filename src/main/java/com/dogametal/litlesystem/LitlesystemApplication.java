09f5d5a8a46f5e6f28711991234e23a975008c43
Douglas Silva <dogametal@hotmail.com>
2020-10-26 05:34:08 -0300

    Project Cretated

---
+++ src/main/java/com/dogametal/litlesystem/LitlesystemApplication.java
@@ -0,0 +1,13 @@
+package com.dogametal.litlesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitlesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitlesystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dogametal/litlesystem/LitlesystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dogametal.litlesystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LitlesystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
