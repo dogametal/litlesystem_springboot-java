54042557501294a57b8a20fb1f9e6c4c72f25794
Douglas Silva <dogametal@hotmail.com>
2020-10-28 06:10:01 -0300

    Service layer, component registration

--- src/main/java/com/dogametal/litlesystem/resources/UserResource.java
+++ src/main/java/com/dogametal/litlesystem/resources/UserResource.java
@@ -1,22 +1,41 @@
 package com.dogametal.litlesystem.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.dogametal.litlesystem.entities.User;
+import com.dogametal.litlesystem.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;	
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
+	public ResponseEntity<List<User>> findAll(){
 		//add L front long type 
-		User u = new User(1L, "Douglas Silva", "dogametal@hotmail.com", "11-99633 9446", "1234");
-		return ResponseEntity.ok().body(u);
+		/*
+		User u = new User(1L, "Douglas Silva Friend", "dogametal@hotmail.com", "11-99633 9446", "1234");
+		*/
+		List<User> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	//framework understand url add @pathvariable
+	public ResponseEntity<User> findById(@PathVariable Long id ){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+		
 	}
 	
 }
---
+++ src/main/java/com/dogametal/litlesystem/services/UserService.java
@@ -0,0 +1,27 @@
+package com.dogametal.litlesystem.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dogametal.litlesystem.entities.User;
+import com.dogametal.litlesystem.repositories.UserRepository;
+
+//Could be @Component but this is more explanation
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
